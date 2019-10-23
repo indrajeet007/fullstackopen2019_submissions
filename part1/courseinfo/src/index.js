7b2e9e13a39db96b77b6efd002cb13445cb112de
indrajeet007 <indrajeet.0072@gmail.com>
2019-10-23 15:57:19 +0530

    step2 submission done

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -10,11 +10,22 @@ const Header = (props) => {
     )
 }
 
+// Part component
+const Part = (props) => {
+    return (
+        <div>
+           <p>{props.part} {props.exercises}</p>
+        </div>
+    )
+}
+
 // Content component
 const Content = (props) => {
     return (
         <div>
-           <p>{props.part} {props.exercises}</p>
+           <Part part={props.part} exercises={props.exercises}/>
+           <Part part={props.part} exercises={props.exercises}/>
+           <Part part={props.part} exercises={props.exercises}/>
         </div>
     )
 }
@@ -44,8 +55,6 @@ const App = () => {
         <Header course={course} />
 
         <Content part={part1} exercises={exercises1} />
-        <Content part={part2} exercises={exercises2} />
-        <Content part={part3} exercises={exercises3} />
 
         <Total exercises={exercises1 + exercises2 + exercises3} />
     </div>
