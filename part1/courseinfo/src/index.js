6ebf9a788ed9607dcfdb5bb935de78d29dc1f2af
indrajeet007 <indrajeet.0072@gmail.com>
2019-10-23 16:21:35 +0530

    step3 submission done

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -42,21 +42,27 @@ const Total = (props) => {
 // main App component
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+    const course = 'Half Stack application development'
+    const part1 = {
+      name: 'Fundamentals of React',
+      exercises: 10
+    }
+    const part2 = {
+      name: 'Using props to pass data',
+      exercises: 7
+    }
+    const part3 = {
+      name: 'State of a component',
+      exercises: 14
+    }
 
   return (
     <div>
         <Header course={course} />
 
-        <Content part={part1} exercises={exercises1} />
+        <Content part={part1.name} exercises={part1.exercises} />
 
-        <Total exercises={exercises1 + exercises2 + exercises3} />
+        <Total exercises={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
