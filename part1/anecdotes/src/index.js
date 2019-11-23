b5847cae110dba935a330b240cd7c2a5b7f9d4e7
indrajeet007 <indrajeet.0072@gmail.com>
2019-11-23 20:57:37 +0530

    added submission work for 1.12* step 1

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Stateless components
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Main app component
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+
+  // calculate random anecdote index value
+  const calRandomAnec = (props) => {
+      const randomAnec = Math.floor(Math.random() * props.anecdotes.length)
+      
+      // save index value to selected state
+      setSelected(randomAnec)
+  }
+
+  return (
+    <div>
+        {selected === 0 ? (<div>{props.anecdotes[selected]}</div>) : <div>{props.anecdotes[selected]}</div>}
+        <Button onClick={() => calRandomAnec(props)} text="next anecdote"/>
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
