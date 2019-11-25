4f01f929da54d5c9793d8015ab4dd36f3d609508
indrajeet007 <indrajeet.0072@gmail.com>
2019-11-25 17:21:31 +0530

    added submission work for 1.13* step2

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -7,6 +7,16 @@ const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 // Main app component
 const App = (props) => {
   const [selected, setSelected] = useState(0)
+  const [votes, setVote] = useState(
+    {
+      "0": 0,
+      "1": 0,
+      "2": 0,
+      "3": 0,
+      "4": 0,
+      "5": 0,
+    }
+  )
 
   // calculate random anecdote index value
   const calRandomAnec = (props) => {
@@ -16,10 +26,29 @@ const App = (props) => {
       setSelected(randomAnec)
   }
 
+  // calculate votes for displayed anecdotes
+  const calAnecVotes = () => {
+    const copyVotes = votes;
+    let selectedString = selected.toString();
+    // console.log("selected", copyVotes[selectedString])
+
+    copyVotes[selectedString] = votes[selectedString] + 1;
+    
+    setVote({
+      ...copyVotes
+    })
+
+    // console.log('Selected: ', selected)
+    console.log('Votes', votes)
+  }
+
   return (
     <div>
         {selected === 0 ? (<div>{props.anecdotes[selected]}</div>) : <div>{props.anecdotes[selected]}</div>}
-        <Button onClick={() => calRandomAnec(props)} text="next anecdote"/>
+        <span>has {votes[selected]} votes</span>
+        <br />
+        <Button onClick={() => calAnecVotes()} text='votes'/>
+        <Button onClick={() => calRandomAnec(props)} text='next anecdote'/>
     </div>
   )
 }
