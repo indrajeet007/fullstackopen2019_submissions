130ea06e45137ba5d406e31861978bbc05b65540
indrajeet007 <indrajeet.0072@gmail.com>
2019-11-28 14:12:45 +0530

    added total component to display total count of exercises

---
+++ part2/course-contents/src/components/Total.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// Total component
+const Total = ({ total }) => {
+    return (
+        <div>
+            <p><strong>total of {total} exercises</strong></p>
+        </div>
+    )
+}
+
+export default Total
