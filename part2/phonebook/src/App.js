ee1ddae925933340433b9b86e367ac7a7eca0265
indrajeet007 <indrajeet.0072@gmail.com>
2019-12-19 15:34:46 +0530

    added submission work for 2.11 The Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 import Persons from './components/Persons'
+import axios from 'axios'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' },
-    { name: 'IND', number: '123' }
-  ])
+  const [ persons, setPersons ] = useState([])
   
   const [ showAll, setShowAll ] = useState(true)
   const [ filteredNameInput, setFilteredNameInput ]  = useState('')
+
+  useEffect(() => {
+    // console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        // console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
   
   const _pushToArr = (value) => {
     let updatedPersons = persons.concat(value)
