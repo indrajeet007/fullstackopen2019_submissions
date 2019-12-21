b43d10f31bc00d05f0da3c3b26a894fc306f27c5
indrajeet007 <indrajeet.0072@gmail.com>
2019-12-21 17:04:16 +0530

    added submission work for 2.12 Data for countries, step1

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Search from './components/Search';
+import DisplayCountries from './components/DisplayCountries';
 
-function App() {
+const App = () => {
+  const [ countries, setCountries ] = useState([])
+  const [ filteredCountry, setFilteredCountry ] = useState([])
+
+  let test = "Too many matches, specify another filter"
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        console.log('promise fulfilled')
+        console.log('response: ', response.data)
+        const countriesList = response.data
+        setCountries(countriesList)
+      })
+  }, [])
+
+  const _handleSearch = ({ event, filterByCountry }) => {
+    console.log(event.target.value)
+    // if(event.target.value === '') {
+    //   console.log('in here!')
+    //   setFilteredCountry(null)
+    // }
+    setFilteredCountry(filterByCountry)
+  }
+
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Search countries={countries} handleInput={_handleSearch}/>
+      {
+        filteredCountry.length < 10 ? (<DisplayCountries countries={filteredCountry} filteredCountryLength={filteredCountry.length}/>) : (<p>{test}</p>)
+      }
     </div>
   )
 }
