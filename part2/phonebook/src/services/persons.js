086740befae79d8de78d38936a7a55ed743d225a
indrajeet007 <indrajeet.0072@gmail.com>
2019-12-24 16:14:59 +0530

    added submission work for 2.16 Phonebook step8 separate service module

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -12,8 +12,8 @@ const create = newObject => {
 }
 
 const update = (id, newObject) => {
-    const request = axios.put(`${baseUrl}/${id}`, newObject)
-    return request.then(response => response.data)
+    // const request = axios.put(`${baseUrl}/${id}`, newObject)
+    // return request.then(response => response.data)
 }
 
 export default { getAll, create, update }
